ced7d19f0c1704b233025e9467c8f2f273e64501
spuertaf <68928400+spuertaf@users.noreply.github.com>
2022-04-14 19:00:05 -0500

    mejora en la doc

--- mongodb.py
+++ mongodb.py
@@ -1,9 +1,9 @@
 
-'''modulo para escribir datos en una coleccion de mongodb
-nombre de la base de datos: livingLab
-nombre de las colecciones: victron, sensors 
-    -> victron, es la coleccion que almacena los datos de la CEIBA
-    -> sensors, es la coleccion que almacena los datos de los sensores de temperatura y humedad.'''
+'''Modulo para escribir datos en una coleccion mongodb
+   nombre de la base de datos: livingLab
+   nombre de las colecciones: victron, sensors 
+   -> victron, es la coleccion que almacena los datos de la CEIBA
+   -> sensors, es la coleccion que almacena los datos de los sensores de temperatura y humedad.'''
 
 
 import db_access as db
@@ -11,19 +11,53 @@ import pymongo
 
 
 class Mongodb(db.Db_access):
+    """Hereda de un DAO: Data Access Object; Representa un objeto para 
+       leer datos de una coleccion MongoDB
+       
+       Attributes:
+        port (int): [Puerto de la base de datos para lectura/escritura]
+        db_name (str): [Nombre de la base de datos]
+        host (str): [Host de la base de datos]
+        collection_name (str): [Nombre de la coleccion MongoDB de la cual se van a leer datos]
+       """
+    
+    
 
-    def __init__(self, port, db_name, host, collection_name):
+    def __init__(self, port: int, db_name: str, host: str, collection_name: str) -> None:
+        """Inicializa el objeto de tipo Mongodb
+        
+        Args:
+           port (int): [Puerto de la base de datos para lectura/escritura]
+            db_name (str): [Nombre de la base de datos]
+            host (str): [Host de la base de datos]
+            collection_name (str): [Nombre de la coleccion MongoDB de la cual se van a leer datos] 
+        """
         super().__init__(port = port, db_name = db_name, host=host)
         self.__collection_name = collection_name
 
 
-    def get_collection_name(self):
+    def get_collection_name(self) -> str:
+        """"Retorna el nombre de la coleccion del objeto tipo Mongodb
+        
+        Args:
+            self(Mongodb)
+            
+        Returns:
+            str
+        """
         return self.__collection_name
 
 
     def connect_to_mongo(self)->pymongo.collection.Collection:
-        '''metodo para establecer una conexion a la base de datos, 
-            retorna el cliente para poder hacer CRUD en una coleccion de la base de datos'''
+        '''Establece una conexion con la base de datos
+        
+        Args:
+            self(Mongodb)
+        
+        Returns:
+            mongo_collection(pymongo.collection.Collection): [Cliente para poder hacer
+            CRUD en una coleccion de la base de datos]
+        '''
         try:
             mongo_client = pymongo.MongoClient(self.get_host(),self.get_port()) #cliente de la base de datos
             mongo_collection = mongo_client[self.get_db_name()][self.get_collection_name()] #coleccion en la que escribir los datos
@@ -32,10 +66,14 @@ class Mongodb(db.Db_access):
             print(error)
 
 
-    def write_to_mongo(self, db_entry:dict, date_hour_value:tuple):
-        '''Funcion para escribir datos tanto de CEIBA como de sensores; recibe como parametros
-        la fecha, hora, etiqueta, valor, nombre de la base de datos y nombre de la coleccion
-        en donde se guardaran los datos.'''
+    def write_to_mongo(self, db_entry:dict, date_hour_value:tuple) -> None:
+        '''Escribir datos a una coleccion MongoDB
+        
+        Args:
+            self(Mongodb)
+            db_entry(dict): [Diccionario con datos a montar en la coleccion MongoDB]
+            date_hour_value(tuple): [Tupla con fecha, hora, valor]
+       '''
 
         db_entry['fecha'] = date_hour_value[0]
         db_entry['hora'] = date_hour_value[1]
