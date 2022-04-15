cf4b3348e237dee849e903f8391d93a6b76bf27c
spuertaf <68928400+spuertaf@users.noreply.github.com>
2022-04-14 19:21:23 -0500

    mejora en la doc

--- influx_database.py
+++ influx_database.py
@@ -7,13 +7,32 @@ from influxdb import InfluxDBClient  #libreria para acceso a los datos de influx
 
 
 class Influxdb(db.Db_access):
+    '''Hereda de un DAO: Data Access Object; Representa un objeto para lectura
+    de la base de datos InfluxDBç
     
-    '''patron de diseño singleton'''
+    Args:
+        instance (Influxdb): [Si hay un objeto Influxdb ya creado; si no 
+        hay ningun objeto Influxdb creado es None]
+        port (int): [Puerto de la base de datos para lectura/escritura]
+        db_name (str): [Nombre de la base de datos]
+        host (str): [Host de la base de datos]
+        '''
+    
     __instance = None #si no hay objetos creados = None
                       #si hay objetos creados = objeto Influxdb creado
-    
-    def __new__(cls, port:int, db_name:str, host:str):
-        if Influxdb.__instance is None: #si no hay ninguna instacia creada
+                      #patron de diseño Singleton
+    def __new__(cls, port:int, db_name:str, host:str) -> None:
+        """Crea el objeto de tipo Influxdb
+        
+        Args:
+            instance (Influxdb): [Si hay un objeto Influxdb ya creado; si no 
+            hay ningun objeto Influxdb creado es None]
+            port (int): [Puerto de la base de datos para lectura/escritura]
+            db_name (str): [Nombre de la base de datos]
+            host (str): [Host de la base de datos] 
+        """
+        if Influxdb.__instance is None: #si no hay ninguna instacia creada;
+                                        #patron de diseño Singleton
             Influxdb.__instance = object.__new__(cls)
         Influxdb.__instance.port = port
         Influxdb.__instance.db_name = db_name
@@ -22,7 +41,17 @@ class Influxdb(db.Db_access):
     
     
     def read_influxdb_data(self, label_to_search:str)->list:
-        '''metodo para obtener todos los datos de una etiqueta almacenada en influxdb.'''
+        '''Obtiene todos los datos de una etiqueta almacenada en la base de datos 
+           Influxdb
+        
+        Args:
+            self (Influxdb)
+            label_to_search (str): [Etiqueta a ser buscada en la base de datos]
+        
+        Returns:
+            all_label_data (list): [Lista con todos los datos hallados de
+            label_to_search]
+        '''
 
         try: #intenta obtener los datos de la base de datos
             influxdb_client = InfluxDBClient(host = self.get_host(), port = self.get_port(), database = self.get_db_name()) #crear el cliente para la recoleccion de los datos
@@ -34,8 +63,16 @@ class Influxdb(db.Db_access):
 
 
     def get_last_label_data(self, label_to_search:str)->tuple:
-        '''metodo para obtener y retornar el ultimo dato con la fecha y hora respectivas 
-        almacenado en influxDB de todas las etiquetas'''
+        """Obtiene y retorna el ultimo dato escrito en la base de datos  
+           de una etiqueta dada
+         
+         Args:
+            label_to_search (str): [Etiqueta a ser buscada en la base de datos]
+         
+         Returns:
+            date_hour_value (tuple): [Tupla que contiene la fecha, hora y valor del
+            ultimo dato registrado en la base de datos de la label_to_search]
+         """
 
         all_label_data = self.read_influxdb_data(label_to_search)
         all_label_data_list = all_label_data['series'] 
